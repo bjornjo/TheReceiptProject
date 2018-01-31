391f2c891fdfb1720658fcf5a2c33ecaddd3a4e2
Bjørn Reitzer <johbjo13@gmail.com>
2018-01-31 19:59:53 +0100

    Hot Module Reloading enabled

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,52 @@
 const path = require('path');
-
 const webpack = require('webpack');
-const CompressionPlugin = require('compression-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackLoggingPlugin = require('webpack-logging-plugin');
+const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 
 const srcDir = path.join(__dirname, 'views');
 
 module.exports = {
-    entry: path.join(srcDir, 'index.js'),
+    entry: [
+        'webpack-hot-middleware/client', path.join(srcDir, 'index.js')
+    ],
     output: {
         path: path.join(__dirname, 'public', 'static'),
         filename: 'bundle.js',
         publicPath: '/static/'
     },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                presets: ['react', 'es2015', 'stage-0']
-            },
-            include: srcDir
-        }, {
-            test: /\.html$/,
-            loader: 'html-loader?attrs[]=video:src'
-        }, {
-            test: /\.(png|jpg|svg)$/,
-            loader: 'url-loader?limit=8192'
-        }, {
-            test: /\.scss$/,
-            loaders: ["style-loader", "css-loader", "sass-loader"]
-        }, {
-            test: /\.svg$/,
-            loader: 'babel-loader!svg-react-loader'
-        }]
-    },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin(),
-        new webpack.optimize.AggressiveMergingPlugin(),
-        new CompressionPlugin({
-            asset: "[path].gz[query]",
-            algorithm: "gzip",
-            test: /\.js$|\.css$|\.html$/,
-            threshold: 10240,
-            minRatio: 0.8
+        new HtmlWebpackPlugin({template: 'views/index.html'}),
+        new webpack.HotModuleReplacementPlugin(),
+        new WebpackLoggingPlugin({
+            formatError: (err) => err,
+            formatStats: (stats) => formatWebpackMessages(stats.toJson({}, true)),
+            successCallback: () => console.log('App is running at: http://localhost:3000/')
         })
-    ]
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                },
+                include: srcDir
+            }, {
+                test: /\.html$/,
+                loader: 'html-loader?attrs[]=video:src'
+            }, {
+                test: /\.(png|jpg|svg)$/,
+                loader: 'url-loader?limit=8192'
+            }, {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }, {
+                test: /\.svg$/,
+                loader: 'babel-loader!svg-react-loader'
+            }
+        ]
+    }
 };
